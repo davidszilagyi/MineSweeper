e8d8c7ec49cdfe62be5ad71b4d457bb3ac28c165
David Szilagyi <szilagyidav@gmail.com>
2017-07-14 14:31:32 +0200

    added to start a fully random game defined by the user

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 import app.FieldGenerator;
 import app.Game;
+import java.util.Scanner;
 
 /**
  * Created by David Szilagyi on 2017. 07. 13..
  */
 public class Main {
     public static void main(String[] args) {
-        FieldGenerator.generateAndWrite(20, 20, 45);
-        Game game = new Game("src/fields/20x20.txt");
+        System.out.print("Add the height: ");
+        int height = new Scanner(System.in).nextInt();
+        System.out.print("Add the width: ");
+        int width = new Scanner(System.in).nextInt();
+        System.out.print("Add the maximum bombs number: ");
+        int maxBombs = new Scanner(System.in).nextInt();
+        FieldGenerator.generateAndWrite(height, width, maxBombs);
+        Game game = new Game(String.format("src/fields/%dx%d.txt", height, width));
         game.gameStart();
-//        app.Mine mine35 = new app.Mine("src/fields/3x5.txt");
-//        app.Mine mine44 = new app.Mine("src/fields/4x4.txt");
-//        app.Mine mine1010 = new app.Mine("src/fields/10x10.txt");
-//        mine35.start();
-//        mine44.start();
-//        mine1010.start();
-//        app.FieldGenerator.generateAndWrite(15, 15);
-//        app.Mine mine1515 = new app.Mine("src/fields/15x15.txt");
-//        mine1515.start();
     }
 }
