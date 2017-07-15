0f808346fbdf67fc1a4468f9f782373346d11a54
David Szilagyi <szilagyidav@gmail.com>
2017-07-15 18:55:18 +0200

    Added a RunJar class to run the jar file which is created

---
+++ src/RunJar.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * Created by David Szilagyi on 2017. 07. 15..
+ */
+public class RunJar {
+    public static void main (String [] args) throws IOException, InterruptedException, URISyntaxException {
+        Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()){
+            String filename = RunJar.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
+        }else{
+            Main.main(new String[0]);
+            System.out.println("Program has ended, please type 'exit' to close the console");
+        }
+    }
+}
