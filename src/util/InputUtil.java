87f791db2140a5bf886b262b07f2006beee31f86
David Szilagyi <szilagyidav@gmail.com>
2017-07-15 18:43:21 +0200

    Created InputUtil class for check and control user inputs

---
+++ src/util/InputUtil.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+/**
+ * Created by David Szilagyi on 2017. 07. 15..
+ */
+public class InputUtil {
+    private static Scanner sc = new Scanner(System.in);
+    
+    public static <T> T checkInput(String text, Class<T> cls) {
+        boolean correct = false;
+        while (!correct) {
+            System.out.print(text);
+            String input = sc.next();
+            try {
+                Method method = cls.getMethod("valueOf", String.class);
+                return cls.cast(method.invoke(null, input));
+            } catch (ClassCastException e) {
+                System.out.println("Wrong input!");
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                System.out.println("Wrong input!");
+            }
+        }
+        return null;
+    }
+
+    public static boolean checkPosition(int number1, int number2) {
+        return number1 < number2 ? true: false;
+    }
+}
