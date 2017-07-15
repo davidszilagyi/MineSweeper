659bb39359dc69dad06b80af2ba6d1c19230f420
David Szilagyi <szilagyidav@gmail.com>
2017-07-15 23:47:40 +0200

    Changed bombs text

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int height = InputUtil.checkInput("Add the height: ", Integer.class);
         int width = InputUtil.checkInput("Add the width: ", Integer.class);
-        int bombs = InputUtil.checkInput("Add the max bombs number: ", Integer.class);
+        int bombs = InputUtil.checkInput("Add max bombs: ", Integer.class);
         FieldGenerator.generate(height, width, bombs);
         Game game = new Game(FieldGenerator.getBoard());
         game.gameStart();
