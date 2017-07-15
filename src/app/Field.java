fd3a6d70ed8f71634a5102612afbc8ae987d99e4
David Szilagyi <szilagyidav@gmail.com>
2017-07-15 23:42:13 +0200

    Print numbers for columns and rows

--- src/app/Field.java
+++ src/app/Field.java
@@ -13,11 +13,31 @@ public abstract class Field {
 
     public static void showBoard(Object[][] board, int height, int width) {
         System.out.println("\nThe board:");
+        int lengthX = String.valueOf(height).length();
+        int lengthY = String.valueOf(width).length();
+        if (lengthX % 2 == 0) {
+            lengthX++;
+        }
+        if (lengthY % 2 == 0) {
+            lengthY++;
+        }
+        printHeader(width, lengthX, lengthY);
         for (int x = 0; x < height; x++) {
+            System.out.printf("%-" + lengthX + "d", x + 1);
             for (int y = 0; y < width; y++) {
-                System.out.print(board[x][y] + " ");
+                System.out.printf("%" + lengthY + "s", board[x][y]);
             }
             System.out.println();
         }
+    }
+
+    private static void printHeader(int width, int lengthX, int lengthY) {
+        for (int i = 0; i < width; i++) {
+            if (i == 0) {
+                System.out.printf("%" + lengthX + "s", " ");
+            }
+            System.out.printf("%" + lengthY + "d", i + 1);
+        }
+        System.out.println();
     }
 }
