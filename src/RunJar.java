a8c60661a5809a9cd336533c567504c87d4dd886
David Szilagyi <szilagyidav@gmail.com>
2017-07-17 09:56:14 +0200

    ignoring %20 when running the jar file

--- src/RunJar.java
+++ src/RunJar.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.io.Console;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 
 /**
  * Created by David Szilagyi on 2017. 07. 15..
@@ -10,7 +11,7 @@ public class RunJar {
     public static void main (String [] args) throws IOException, InterruptedException, URISyntaxException {
         Console console = System.console();
         if(console == null && !GraphicsEnvironment.isHeadless()){
-            String filename = RunJar.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            String filename = URLDecoder.decode(RunJar.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6), "UTF-8");
             Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
         }else{
             Main.main(new String[0]);
